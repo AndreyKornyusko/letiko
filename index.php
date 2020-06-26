18d6206deeb16d58d9fd1473ff8c537233347b25
Andrey Kornyushko <kap86@ukr.net>
2020-06-26 13:29:10 +0300

    changed index php

--- index.php
+++ index.php
@@ -1,25 +1,38 @@
 <?php
 
-if(isset($_POST['submit'])){
-$to = "kap86@ukr.net";; // Здесь нужно написать e-mail, куда будут приходить письма
-$from = $_POST['themail']; // this is the sender's Email address
-// $first_name = $_POST['first_name'];
-$email = $_POST['themail'];
+if($isset($_POST['submit']))
+{
+$mailFrom = $_POST['themail'];
+$message = $_POST['themessage'];
 
-$subject = "Форма отправки сообщений с сайта";
-$subject2 = "Copy of your form submission";
-$message = $email . " оставил сообщение:" . "\n\n" . $_POST['themessage'];
-$message2 = "Here is a copy of your message " . $email . "\n\n" . $_POST['themessage'];
+$mailTo = "kap86@ukr.net";
+$headers =  "From: ".$mailFrom;
 
-$headers = "From:" . $from;
-$headers2 = "From:" . $to;
+mail($mailTo,  $message, $headers);
 
-mail($to,$subject,$message,$headers);
-// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-echo "Сообщение отправлено. Спасибо Вам " . $email . ", мы скоро свяжемся с Вами.";
-// echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
-
+header("Location: index.htm");
 }
+
+// if(isset($_POST['submit'])){
+// $to = "kap86@ukr.net";; // Здесь нужно написать e-mail, куда будут приходить письма
+// $from = $_POST['themail']; // this is the sender's Email address
+// // $first_name = $_POST['first_name'];
+// $email = $_POST['themail'];
+
+// $subject = "Форма отправки сообщений с сайта";
+// $subject2 = "Copy of your form submission";
+// $message = $email . " оставил сообщение:" . "\n\n" . $_POST['themessage'];
+// $message2 = "Here is a copy of your message " . $email . "\n\n" . $_POST['themessage'];
+
+// $headers = "From:" . $from;
+// $headers2 = "From:" . $to;
+
+// mail($to,$subject,$message,$headers);
+// // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+// echo "Сообщение отправлено. Спасибо Вам " . $email . ", мы скоро свяжемся с Вами.";
+// // echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+
+// }
 
 ?>
 
